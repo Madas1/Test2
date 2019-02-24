50d0226ffaf5aa6c24a7018d19be2122bfafd412
Madas <bhavani.lmo@gmail.com>
2019-02-23 22:20:24 -0700

    Switch Case Calculator

---
+++ index.js
@@ -0,0 +1,48 @@
+
+function calculator(){
+
+    switch(){
+
+        case 1 :
+        
+        var a = document.getElementById('a').value;
+        var b = document.getElementById('b').value;
+
+        Var c = parseInt(a)+parseInt(b);
+        console.log(c);
+        break;
+
+        Case 2: 
+        var a = document.getElementById('a').value;
+        var b = document.getElementById('b').value;
+
+        Var c = parseInt(a)-parseInt(b);
+        console.log(c);
+        break;
+
+        Case 3: 
+        var a = document.getElementById('a').value;
+        var b = document.getElementById('b').value;
+
+        Var c = parseInt(a)*parseInt(b);
+        console.log(c);
+        break;
+
+        Case 4:
+        var a = document.getElementById('a').value;
+        var b = document.getElementById('b').value;
+
+        Var c = parseInt(a)/parseInt(b);
+        console.log(c);
+        break;
+
+        case 5:
+        var a = document.getElementById(a).value;
+        var b = document.getElementById(b).value;
+
+        var c = parseInt(a)%parseInt(b);
+        console.log(c);
+        break;
+
+}
+}
